e40d2d967fcf6dd87f1b2c6128270a9777c91ea7
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2023-12-13 16:06:51 +0800

    updated serverless api feedback

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "message": "Go Serverless v3.0! Martin Leong created a function in serverless executed successfully!",
         "input": event,
     }
 
